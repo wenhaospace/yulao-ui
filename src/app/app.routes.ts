6f5d40b5d3a6cb1d713459a9110f6aba835de341
ZhangWenhao <wbfw-zwh@eastmoney.com>
2025-12-22 13:23:50 +0800

    增加主页显示以及github链接

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,7 +15,7 @@ export const routes: Routes = [
     },
     {
         path: '',
-        redirectTo: 'login',
+        redirectTo: 'home',
         pathMatch: 'full',
     },
     {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,17 +3,34 @@ import { HttpService } from '../../services/http/http.service';
 import { API_ENDPOINTS } from '../../configuration/apis/river_endpoint_v1';
 import { HttpResponse } from '../../entity/http/HttpResponse';
 import { User } from '../../entity/User';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   userList: User[] = [];
+
+   personalInfo = {
+    name: '张文浩',
+    title: '全栈工程师',
+    description: '我是一名全栈工程师，擅长前端和后端开发，热爱编程和技术创新。',
+    contact: 'superwenhao.511@gmail.com'
+  };
+
+  projects = [
+    { title: '智能翻译', description: '基于Qwen3实现的多语言翻译应用', link: '/translator' },
+    { title: 'Project B', description: 'A description of Project B.', link: '/' },
+    { title: 'Project C', description: 'A description of Project C.', link: '/' },
+    { title: '翻译', description: '基于Qwen3实现的多语言翻译应用', link: '/translator' },
+    { title: 'Project B', description: 'A description of Project B.', link: '/' },
+    { title: 'Project C', description: 'A description of Project C.', link: '/' }
+  ];
 
   constructor(private httpService: HttpService) { }
 
