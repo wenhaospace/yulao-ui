8385d32406cc5c235147c804357d48f5580838c7
ZhangWenhao <wbfw-zwh@eastmoney.com>
2025-12-23 16:42:00 +0800

    更新项目描述

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -30,6 +30,7 @@ export class HomeComponent {
     { title: '农作物辅助种植(调研中)', description: '上传农作物图片，经过图片模型处理分析生长状态， 给出最佳养护指导', link: '/' },
     { title: '公文简报写作(调研中)', description: '基于给定资料和公文模板，快速生成公务员简报', link: '/' },
     { title: '网页阅读(搁置)', description: '总结页面主要内容，可用于快速学习或查阅技术文档 - 备注： 中文页面布局过于紧凑， 需要先做数据清洗后再调用大模型分析', link: '/' },
+    { title: '代码片段重构转换(开发中)', description: '用于重构相同逻辑的代码,SQL或格式, 例如： Java实体切换成Python实体; Java实体切换成对应的Json实体 ', link: '/' },
   ];
 
   constructor(private httpService: HttpService) { }
