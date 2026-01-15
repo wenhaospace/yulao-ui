42f40a081a2a9b5f025419a37e04ad209623663b
ZhangWenhao <wbfw-zwh@eastmoney.com>
2026-01-15 10:49:04 +0800

    给资料系统增加导航管理

--- src/app/configuration/properties.ts
+++ src/app/configuration/properties.ts
@@ -1,3 +1,5 @@
-export const baseUrl = 'http://wenhaospace.site/api/';
+// export const baseUrl = 'http://wenhaospace.site/api/';
+
+export const baseUrl = 'http://localhost:9200/';
 
 export const agentUrl = 'http://47.245.100.81:8000';
--- src/app/pages/my-file/my-file.component.ts
+++ src/app/pages/my-file/my-file.component.ts
@@ -1,19 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-my-file',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, RouterModule],
   templateUrl: './my-file.component.html',
   styleUrl: './my-file.component.css'
 })
 export class MyFileComponent {
 
-  
+  // 模拟权限（实际可从服务或 token 解析）
+  canViewList = true;
+  canManage = this.hasPermission('manage_library');
+
   constructor() {
     
   }
+
+  private hasPermission(role: string): boolean {
+    // 示例：从 localStorage、AuthService 等获取权限
+    const permissions = ['view_library', 'manage_library'];
+    return permissions.includes(role);
+  }
+  
+
 
     
 }
